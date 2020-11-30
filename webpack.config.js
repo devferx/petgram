bb8fc76ca27f95abae8cbc76e9ac893b4e9b6583
devferx <ferkki@hotmail.com>
2020-11-30 10:35:27 -0400

    Clase Instalación de React y Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+const path  = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
       {
         test: /\.html$/,
         use: ['html-loader']
@@ -12,6 +29,8 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
   ]
 }
