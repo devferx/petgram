b2c2cdf8de80b1a93c2d7c9508d4d0ce6a8199cc
devferx <ferkki@hotmail.com>
2020-12-01 09:01:05 -0400

    Clase SVGR: de SVG a componente de ReactJS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </div>
