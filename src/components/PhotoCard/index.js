ffad026e88cf0a2c77ac3c398b5f589f6b2f3ac0
devferx <ferkki@hotmail.com>
2020-12-02 11:48:17 -0400

    🧠 Clase Usando el localStorage para guardar los likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { Article, ImgWrapper, Img, Button } from './styles'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const [show, setShow] = useState(false)
   const element = useRef(null)
+  const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return JSON.parse(like)
+    } catch (error) {
+      return false
+    }
+  })
 
   useEffect(function () {
     Promise.resolve(
@@ -16,7 +25,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     ).then(() => {
       const observer = new window.IntersectionObserver((entries) => {
         const { isIntersecting } = entries[0]
-        console.log(isIntersecting)
         if (isIntersecting) {
           setShow(true)
           observer.disconnect()
@@ -25,6 +33,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       observer.observe(element.current)
     })
   }, [element])
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   return (
     <Article ref={element}>
@@ -36,7 +55,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-            <Button><MdFavoriteBorder size='32px' /> {likes} likes!</Button>
+            <Button onClick={() => setLocalStorage(!liked)}><Icon size='32px' /> {likes} likes!</Button>
           </>
       }
 
