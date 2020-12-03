ebc8a874250b6a15f8f1b8913f52ab641bfa3523
devferx <ferkki@hotmail.com>
2020-12-02 21:52:10 -0400

    Clase Usando las mutaciones con los likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
+
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
-import { Article, ImgWrapper, Img, Button } from './styles'
+
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,8 +13,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
   const [show, element] = useNearScreen()
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -23,7 +24,22 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-            <Button onClick={() => setLiked(!liked)}><Icon size='32px' /> {likes} likes!</Button>
+            <ToggleLikeMutation>
+              {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({
+                    variables: {
+                      input: { id }
+                    }
+                  })
+                  setLiked(!liked)
+                }
+
+                return (<FavButton liked={liked} likes={likes} onClick={handleFavClick} />)
+              }
+            }
+            </ToggleLikeMutation>
           </>
       }
 
