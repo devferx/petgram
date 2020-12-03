bf26144b0951c1babdc260d2abbddcc82c7a8a37
devferx <ferkki@hotmail.com>
2020-12-03 19:26:41 -0400

    Clase Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { Router } from '@reach/router'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
 import { GlobalStyle } from './styles/GlobalStyles'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 const App = () => {
   return (
@@ -18,6 +25,24 @@ const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+                )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+                )
+          }
+      </UserLogged>
       <Navbar />
     </div>
   )
