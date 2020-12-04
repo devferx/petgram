6d0fb713f67cfccfa07d6b8371e8f259c85b9baa
devferx <ferkki@hotmail.com>
2020-12-04 19:13:04 -0400

    Clase Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Router } from '@reach/router'
+import React, { useContext } from 'react'
+import { Redirect, Router } from '@reach/router'
 
-import Context from './context'
+import { Context } from './context'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Logo } from './components/Logo'
@@ -14,6 +15,8 @@ import { Navbar } from './components/Navbar'
 import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
+
   return (
     <div>
       <GlobalStyle />
@@ -21,28 +24,19 @@ export const App = () => {
       {/* <ListOfCategories />
       <ListOfPhotoCards categoryId={6} /> */}
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-                )
-              : (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-                )
-          }
-      </Context.Consumer>
       <Navbar />
     </div>
   )
