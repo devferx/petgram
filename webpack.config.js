1be2509ff1d256e5aca56a5cff0581179f708c65
devferx <ferkki@hotmail.com>
2020-12-05 11:36:31 -0400

    Clase PWA: soporte offline

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable prefer-regex-literals */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -46,6 +48,32 @@ module.exports = {
         {
           src: path.resolve('src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      // clientsClaim: true,
+      // skipWaiting: true,
+      // maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.cloudinary.com|images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp(
+            'https://petgram-server-self.vercel.app'
+          ),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
